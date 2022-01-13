2555a0e92a89c4dc8d9a560998738e04c0977651
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-14 01:43:52 +0900

    add: token追加

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next'
 
 const Index: NextPage = () => {
+  const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN
+  console.log(token)
   return (
     <div className="w-screen h-screen">
       <div className="font-bold text-4xl flex justify-center pt-48">
