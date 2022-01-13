5e1aece0fff194f504832ba6b81658d9e645a77f
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-13 22:57:59 +0900

    refs: 初期値をダークモードに設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <div className="bg-lighttheme dark:bg-darktheme">
         <Header />
         <Component {...pageProps} />
