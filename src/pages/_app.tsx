4a8820bfd2d99b770c7d5dab9268bc55970b6179
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-14 23:57:12 +0900

    add: apollo client追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,39 @@ import type { AppProps } from 'next/app'
 import Header from '../components/organisms/header'
 import Footer from '../components/organisms/footer'
 import { ThemeProvider } from 'next-themes'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from '@apollo/client'
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql'
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+  },
+})
+
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
-      <div className="bg-lighttheme dark:bg-darktheme">
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <div className="bg-lighttheme dark:bg-darktheme">
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,8 +1,21 @@
 import { NextPage } from 'next'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_CURRENT_USER = gql`
+  {
+    viewer {
+      login
+      name
+    }
+  }
+`
 
 const Index: NextPage = () => {
-  const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN
-  console.log(token)
+  const { loading, error, data } = useQuery(GET_CURRENT_USER)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {JSON.stringify(error)}</p>
+  console.log(data)
+
   return (
     <div className="w-screen h-screen">
       <div className="font-bold text-4xl flex justify-center pt-48">
