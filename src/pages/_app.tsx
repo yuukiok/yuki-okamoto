67a6e5c641bc55eba345da30bb96a97a3c9d2994
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-16 23:27:23 +0900

    add: urql実装&apollo削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,31 +3,24 @@ import type { AppProps } from 'next/app'
 import Header from '../components/organisms/header'
 import Footer from '../components/organisms/footer'
 import { ThemeProvider } from 'next-themes'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-} from '@apollo/client'
+import { createClient, Provider } from 'urql'
 
 const GITHUB_BASE_URL = 'https://api.github.com/graphql'
 
-const httpLink = new HttpLink({
-  uri: GITHUB_BASE_URL,
-  headers: {
-    authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+const client = createClient({
+  url: GITHUB_BASE_URL,
+  fetchOptions: () => {
+    return {
+      headers: {
+        authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+      },
+    }
   },
-})
-
-const cache = new InMemoryCache()
-const client = new ApolloClient({
-  link: httpLink,
-  cache,
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
+    <Provider value={client}>
       <ThemeProvider attribute="class" defaultTheme="dark">
         <div className="bg-lighttheme dark:bg-darktheme">
           <Header />
@@ -35,7 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Footer />
         </div>
       </ThemeProvider>
-    </ApolloProvider>
+    </Provider>
   )
 }
 
