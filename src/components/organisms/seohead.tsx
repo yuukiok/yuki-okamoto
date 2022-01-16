b8477098b2b40fcb3cc294281744160d9ae1b13b
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-16 15:19:59 +0900

    add: seo画像追加

--- src/components/organisms/seohead.tsx
+++ src/components/organisms/seohead.tsx
@@ -16,17 +16,20 @@ export const SEOHead: NextPage<Props> = ({ title, description, url }) => {
   const path = router.asPath
   const isBlogPage = /\/blogs\/.+$/.test(path)
 
-  const thumbnail = '/my_icon.jpg'
-  // https://twitter.com/yuki_0206_cs/photo
+  const thumbnail = '/s_my_icon.jpeg'
 
   return (
     <Head>
       <title>{title}</title>
       <meta property="og:locale" content="en" />
-      <meta property="og:title" content="Yuki's portfolio" />
-      <meta property="og:url" content={`https://yukiokamoto.io${url}`} />
+      <meta property="og:title" content="Yuki's Portfolio" />
+      <meta property="og:url" content={`https://yukiok.com${url}`} />
+      <link rel="apple-touch-icon" href="apple-touch-icon.png" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="yuki_0206_cs" />
+      <meta name="twitter:image" content={thumbnail} />
+      <meta property="og:site_name" content="Yuki Okamoto's Portfolio" />
+      <meta property="og:type" content="website" />
       {isBlogPage ? (
         <>
           <meta name="description" content={description} />
