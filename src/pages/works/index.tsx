dc77d5fdacbbc575677c360975a180b777e71dd2
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-21 23:00:38 -0800

    refs: 不要文削除

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,28 +1,14 @@
 import { NextPage } from 'next'
 import { useQuery } from 'urql'
-import { withUrqlClient } from 'next-urql'
-import Image from 'next/image'
-import DayContribution from '../../components/atoms/dayContribution'
 import WeekContributions from '../../components/molecules/weekContributions'
 import Spacer from '../../components/layout/spacer'
 import { useEffect } from 'react'
 
-// const GET_CURRENT_USER = `
-//   query {
-//     viewer {
-//       login
-//       name
-//       bio
-//       avatarUrl
-
-//     }
-//   }
-// `
 const userName = 'YukiOkamoto0206'
 
 const GET_CURRENT_USER = `
   query {
-    user(login: "YukiOkamoto0206"){
+    user(login: ${userName}){
       contributionsCollection {
         contributionCalendar {
           colors
@@ -50,17 +36,9 @@ const Index: NextPage = () => {
   if (fetching) return <p>Loading...</p>
   if (error) return <p>Error: {JSON.stringify(error)}</p>
   const weeksWeed = data.user.contributionsCollection.contributionCalendar.weeks
-  const numWeeks = weeksWeed.length // 53
-  // console.log(weeksWeed)
-  const numDays = weeksWeed[0].contributionDays.length // 7
-  // console.log(numDays)
-  // const dayWeed = weekWeed.contributionDays[0]
-  // console.log(dayWeed)
-  // const url = data.viewer.avatarUrl
-  const num = 10
 
   return (
-    <div className="">
+    <div>
       <div className="font-bold text-4xl flex justify-center pt-48">
         Under Construction
       </div>
