59f0eb4b6030a8b53a1be71086b32b87799d6d08
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-01-21 22:40:12 -0800

    fix: tailwind効かない問題修正

--- src/components/atoms/dayContribution.tsx
+++ src/components/atoms/dayContribution.tsx
@@ -17,21 +17,22 @@ const DayContribution: React.FC<Props> = ({
   contributionCount,
   date,
 }) => {
+  const name_of_class = 'relative h-4 w-4 my-1 ml-1 rounded-sm'
   return (
     <div>
-      <div
-        className={`peer relative h-4 w-4 my-1 ml-1 bg-green-${
-          color == Color.NONE
-            ? '0'
-            : color == Color.FIRST_QUARTILE
-            ? '100'
-            : color == Color.SECOND_QUARTILE
-            ? '200'
-            : color == Color.THIRD_QUARTILE
-            ? '300'
-            : '400'
-        } rounded-sm`}
-      ></div>
+      <div className="peer">
+        {color == Color.NONE ? (
+          <div className={`bg-green-50 ${name_of_class}`} />
+        ) : color == Color.FIRST_QUARTILE ? (
+          <div className={`bg-green-100 ${name_of_class}`} />
+        ) : color == Color.SECOND_QUARTILE ? (
+          <div className={`bg-green-200 ${name_of_class}`} />
+        ) : color == Color.THIRD_QUARTILE ? (
+          <div className={`bg-green-300 ${name_of_class}`} />
+        ) : (
+          <div className={`bg-green-400 ${name_of_class}`} />
+        )}
+      </div>
       <div className="absolute invisible peer-hover:visible z-10 bg-gray-400 px-1 py-0.5 rounded-md text-sm text-center">
         <div>{contributionCount} contributions</div>
         <div>{date}</div>
