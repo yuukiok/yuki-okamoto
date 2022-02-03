b15daa6f8fefa0876686ee9343b7d13c166bcca3
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-03 01:27:28 -0800

    add: call graphcms from works

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,8 +1,8 @@
 import { GetStaticProps, NextPage } from 'next'
 import { client_works, GET_WORKS_DATA } from '../../lib/urqlClient'
 
-const Index: NextPage<any> = () => {
-  // console.log(data)
+const Index: NextPage<any> = ({ data }) => {
+  console.log(data)
   return (
     <div>
       <div className="font-bold text-4xl flex justify-center pt-48 h-screen">
@@ -14,12 +14,12 @@ const Index: NextPage<any> = () => {
 
 export default Index
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const { data } = await client_works.query(GET_WORKS_DATA).toPromise()
-//   return {
-//     props: {
-//       data,
-//     },
-//     revalidate: 10,
-//   }
-// }
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client_works.query(GET_WORKS_DATA).toPromise()
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10,
+  }
+}
