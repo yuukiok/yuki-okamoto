c6cc60a3d63af686e6fac2b28fea1c770d736d87
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-04 20:20:37 -0800

    add: 型定義

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -6,17 +6,31 @@ import Spacer from '../../components/layout/spacer'
 
 function GraphCMSImageLoader({ src, width }: any) {
   const relativeSrc = (src: any) => src.split('/').pop()
-
   return `https://media.graphcms.com/resize=width:${width}/${relativeSrc(src)}`
 }
-const Index: NextPage<any> = ({ data }) => {
-  const works = data.work
+
+type Props = {
+  work: Work[]
+}
+type Work = {
+  id: string
+  title: string
+  image: Image
+}
+type Image = {
+  url: string
+  width: number
+  height: number
+}
+
+const Index: NextPage<Props> = ({ work }) => {
+  const works = work
   console.log(works)
   return (
     <Spacer>
       <div className="pt-20 mx-auto">
         {works &&
-          works.map((work: any) => {
+          works.map((work: Work) => {
             return (
               <div key={work.id} className="grid-x-2">
                 <Image
@@ -41,9 +55,10 @@ export default Index
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await client_works.query(GET_WORKS_DATA).toPromise()
+  const work = data.work
   return {
     props: {
-      data,
+      work,
     },
     revalidate: 10,
   }
