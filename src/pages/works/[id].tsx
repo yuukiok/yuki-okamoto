faf6e25f2f3311467e56acfee741c4e253e2809e
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-05 14:23:19 -0800

    add: detail page

---
+++ src/pages/works/[id].tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+
+const Index: NextPage<any> = () => {
+  // console.log(data)
+  return (
+    <div>
+      <div className="font-bold text-4xl flex justify-center pt-48 h-screen">
+        Under Construction
+      </div>
+    </div>
+  )
+}
+
+export default Index
