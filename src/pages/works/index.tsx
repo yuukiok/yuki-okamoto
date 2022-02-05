b1dc3d94a3de9cdaf99f82790c4b7ce5668e1a14
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-04 17:54:02 -0800

    add: works画像表示

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,14 +1,39 @@
 import { GetStaticProps, NextPage } from 'next'
 import { client_works, GET_WORKS_DATA } from '../../lib/urqlClient'
 import Work from '../../components/organisms/work'
+import Image from 'next/image'
+import Spacer from '../../components/layout/spacer'
 
+function GraphCMSImageLoader({ src, width }: any) {
+  const relativeSrc = (src: any) => src.split('/').pop()
+
+  return `https://media.graphcms.com/resize=width:${width}/${relativeSrc(src)}`
+}
 const Index: NextPage<any> = ({ data }) => {
   const works = data.work
   console.log(works)
   return (
-    <div className="pt-20">
-      {works && works.map((work: any) => <Work key={work.id} {...work} />)}
-    </div>
+    <Spacer>
+      <div className="pt-20 mx-auto">
+        {works &&
+          works.map((work: any) => {
+            return (
+              <div key={work.id} className="grid-x-2">
+                <Image
+                  loader={GraphCMSImageLoader}
+                  src={work.image.url}
+                  width={300}
+                  height={200}
+                  className="rounded-lg"
+                />
+                <div className="font-semibold text-lg mx-auto">
+                  {work.title}
+                </div>
+              </div>
+            )
+          })}
+      </div>
+    </Spacer>
   )
 }
 
