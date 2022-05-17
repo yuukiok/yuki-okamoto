984093227312ca532cbfab0b3dea10766cc8e99b
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-17 16:44:43 -0700

    add: util file

---
+++ src/utils/carrier.ts
@@ -0,0 +1,37 @@
+import { IconImage } from '../components/molecules/time'
+export const carriers = [
+  {
+    icon: IconImage.COMPANY,
+    title: 'dKomplex',
+    from_date: 'Feb 2022',
+    to_date: 'Present',
+    url: 'https://www.dkomplex.com/',
+    children:
+      'Data Scientist to support the group of a veteran-owned startup by converting business processes and requirements into software updates.',
+  },
+  {
+    icon: IconImage.SCHOOL,
+    title: 'California State University, Monterey Bay',
+    from_date: 'Jan 2022',
+    to_date: 'Present',
+    url: 'https://csumb.edu/',
+    children:
+      'Candidate for Bachelor of Science in Computer Science, expected to graduate in December 2023.',
+  },
+  {
+    icon: IconImage.COMPANY,
+    title: 'Crunchtimer Inc',
+    from_date: 'Jun 2020',
+    to_date: 'Present',
+    url: 'https://www.crunchtimer.jp/',
+    children: 'Front-End and Mobile Application Engineer.',
+  },
+  {
+    icon: IconImage.SCHOOL,
+    title: 'Palomar College',
+    from_date: 'Aug 2019',
+    to_date: 'Dec 2021',
+    url: 'https://www2.palomar.edu/',
+    children: 'Community college majoring Computer Science.',
+  },
+]
