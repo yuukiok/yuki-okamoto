b3843cf57baf5f4bce1ce935e34dd06de551d4bd
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-31 00:59:43 +0900

    refs: new companies

--- src/components/molecules/time.tsx
+++ src/components/molecules/time.tsx
@@ -7,7 +7,7 @@ type Props = {
   title: string
   children: JSX.Element
   from_date: string
-  to_date: string
+  to_date?: string
   url: string
   isDone?: boolean
 }
@@ -57,7 +57,7 @@ export const Time: NextPage<Props> = ({
               </span>
               <div className="inline">{title}</div>
             </a>
-            <div className=" text-sm">{`${from_date} - ${to_date}`}</div>
+            <div className=" text-sm">{`${from_date} ${to_date === undefined ? '' : `- ${to_date}` }`}</div>
             <div className="text-sm font-medium leading-snug tracking-wide whitespace-pre-wrap">
               {children}
             </div>
--- src/utils/carrier.tsx
+++ src/utils/carrier.tsx
@@ -1,18 +1,39 @@
 import { IconImage } from '../components/molecules/time'
 export const carriers = [
-  {
-    icon: IconImage.COMPANY,
-    title: 'dKomplex',
-    from_date: 'Feb 2022',
-    to_date: 'Present',
-    url: 'https://www.dkomplex.com/',
-    children: (
-      <div>
-        Data Scientist to support the group of a veteran-owned startup by
-        converting business processes and requirements into software updates.
-      </div>
-    ),
-  },
+  // {
+  //   icon: IconImage.COMPANY,
+  //   title: 'WealthPark, Inc.',
+  //   from_date: 'Aug 2022',
+  //   to_date: 'Present',
+  //   url: 'https://wealth-park.com/en/lp-product-us/',
+  //   children: (
+  //     <div>
+  //       Front-End Engineer.
+  //     </div>
+  //   )
+  // },
+  // {
+  //   icon: IconImage.COMPANY,
+  //   title: 'Rakuten Group, Inc.',
+  //   from_date: 'July 2022',
+  //   url: 'http://global.rakuten.com/corp/',
+  //   children: (
+  //     <div>
+  //       Fulltime Back-End Engineering internship at <span><a href="https://payment.rakuten.co.jp/en/" className="hover:opacity-50" target="_blank">Rakuten Payment</a></span>.
+  //     </div>
+  //   )
+  // },
+  // {
+  //   icon: IconImage.COMPANY,
+  //   title: 'CyberAgent, Inc.',
+  //   from_date: 'Jun 2022',
+  //   url: 'https://www.cyberagent.co.jp/en/',
+  //   children: (
+  //     <div>
+  //       Fulltime Front-End Engineering internship at <span><a href="https://cam-inc.co.jp/" className="hover:opacity-50"  target="_blank">CAM</a></span>.
+  //     </div>
+  //   )
+  // },
   {
     icon: IconImage.SCHOOL,
     title: 'California State University, Monterey Bay',
@@ -28,9 +49,22 @@ export const carriers = [
   },
   {
     icon: IconImage.COMPANY,
-    title: 'Crunchtimer Inc',
+    title: 'dKomplex, Inc.',
+    from_date: 'Feb 2022',
+    to_date: 'May 2022',
+    url: 'https://www.dkomplex.com/',
+    children: (
+      <div>
+        Data Scientist to support the group of a veteran-owned startup by
+        converting business processes and requirements into software updates.
+      </div>
+    ),
+  },
+  {
+    icon: IconImage.COMPANY,
+    title: 'Crunchtimer, Inc.',
     from_date: 'Jun 2020',
-    to_date: 'Present',
+    to_date: 'May 2022',
     url: 'https://www.crunchtimer.jp/',
     children: <div>Front-End and Mobile Application Engineer.</div>,
   },
