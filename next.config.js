9764faccd39084520e073483b00d525232fcf810
Yuki Okamoto <75482698+yuukiok@users.noreply.github.com>
2024-12-10 21:30:00 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ module.exports = {
   images: {
     domains: ['avatars.githubusercontent.com', 'media.graphcms.com'],
   },
+  async headers() {
+    return [
+      {
+        source: "/piyo/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://d3vinfe0vsz3c9.cloudfront.net" },
+          { key: "Access-Control-Allow-Methods", value: "GET,HEAD,OPTIONS,PUT,POST,PATCH,DELETE" },
+          { key: "Access-Control-Allow-Headers", value: "*" }
+        ]
+      }
+    ]
+  }
 }
